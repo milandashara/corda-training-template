8b7d87ac2a566c82f67571e2d31f7f3f3d20fe09
milan <milan.ashara@thalesgroup.com>
2019-06-28 18:03:11 +0800

    Assignment 6,7,8,9: IOUContract issue command unit test

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -1,8 +1,6 @@
 package net.corda.training.contract
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.training.state.IOUState
 
@@ -25,6 +23,7 @@ class IOUContract : Contract {
         // Add commands here.
         // E.g
         // class DoSomething : TypeOnlyCommandData(), Commands
+        class Issue : TypeOnlyCommandData(), Commands
     }
 
     /**
@@ -36,5 +35,18 @@ class IOUContract : Contract {
         // requireThat {
         //     ...
         // }
+
+        val command = tx.commands.requireSingleCommand<IOUContract.Commands>()
+        when (command.value) {
+            is Commands.Issue -> requireThat {
+                "No inputs should be consumed when issuing an IOU." using (tx.inputs.isEmpty())
+                "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
+                val iou = tx.outputStates.single() as IOUState
+                "A newly issued IOU must have a positive amount." using (iou.amount.quantity > 0)
+                "The lender and borrower cannot have the same identity." using (iou.borrower != iou.lender)
+                "Both lender and borrower together only may sign IOU issue transaction." using
+                        (command.signers.toSet() == iou.participants.map { it.owningKey }.toSet())
+            }
+        }
     }
 }
