1592764df61efd7eea197d386d83071b906c8585
milan <milan.ashara@thalesgroup.com>
2019-07-01 15:41:42 +0800

    Assignment 10,11,12,13: IOUIssueFlow issue command unit test

--- kotlin-source/src/main/kotlin/net/corda/training/flow/IOUIssueFlow.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/flow/IOUIssueFlow.kt
@@ -1,17 +1,12 @@
 package net.corda.training.flow
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.Command
 import net.corda.core.contracts.requireThat
-import net.corda.core.flows.CollectSignaturesFlow
-import net.corda.core.flows.FinalityFlow
-import net.corda.core.flows.FlowLogic
-import net.corda.core.flows.FlowSession
-import net.corda.core.flows.InitiatedBy
-import net.corda.core.flows.InitiatingFlow
-import net.corda.core.flows.SignTransactionFlow
-import net.corda.core.flows.StartableByRPC
+import net.corda.core.flows.*
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.training.contract.IOUContract
 import net.corda.training.state.IOUState
 
 /**
@@ -25,10 +20,37 @@ import net.corda.training.state.IOUState
 class IOUIssueFlow(val state: IOUState) : FlowLogic<SignedTransaction>() {
     @Suspendable
     override fun call(): SignedTransaction {
-        // Placeholder code to avoid type error when running the tests. Remove before starting the flow task!
-        return serviceHub.signInitialTransaction(
-                TransactionBuilder(notary = null)
-        )
+
+        // Step 1. Get a reference to the notary service on our network and our key pair.
+        // Note: ongoing work to support multiple notary identities is still in progress.
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+        // Step 2. Create a new issue command.
+        // Remember that a command is a CommandData object and a list of CompositeKeys
+        val issueCommand = Command(IOUContract.Commands.Issue(), state.participants.map { it.owningKey })
+
+        // Step 3. Create a new TransactionBuilder object.
+        val builder = TransactionBuilder(notary = notary)
+
+
+        // Step 4. Add the iou as an output state, as well as a command to the transaction builder.
+        builder.addOutputState(state, IOUContract.IOU_CONTRACT_ID)
+        builder.addCommand(issueCommand)
+
+
+        // Step 5. Verify and sign it with our KeyPair.
+        builder.verify(serviceHub)
+        val ptx = serviceHub.signInitialTransaction(builder)
+
+
+        val sessions = (state.participants - ourIdentity).map { initiateFlow(it) }.toSet()
+        // Step 6. Collect the other party's signature using the SignTransactionFlow.
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+
+        // Step 7. Assuming no exceptions, we can now finalise the transaction.
+        return subFlow(FinalityFlow(stx, sessions))
+
+
     }
 }
 
@@ -37,15 +59,17 @@ class IOUIssueFlow(val state: IOUState) : FlowLogic<SignedTransaction>() {
  * The signing is handled by the [SignTransactionFlow].
  */
 @InitiatedBy(IOUIssueFlow::class)
-class IOUIssueFlowResponder(val flowSession: FlowSession): FlowLogic<Unit>() {
+class IOUIssueFlowResponder(val flowSession: FlowSession): FlowLogic<SignedTransaction>() {
     @Suspendable
-    override fun call() {
+    override fun call():SignedTransaction {
         val signedTransactionFlow = object : SignTransactionFlow(flowSession) {
             override fun checkTransaction(stx: SignedTransaction) = requireThat {
                 val output = stx.tx.outputs.single().data
                 "This must be an IOU transaction" using (output is IOUState)
             }
         }
-        subFlow(signedTransactionFlow)
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+
+        return subFlow(ReceiveFinalityFlow(otherSideSession = flowSession, expectedTxId = txWeJustSignedId.id))
     }
 }
